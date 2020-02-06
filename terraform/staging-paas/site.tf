15de6141a26bcd4ff3cd6e4b6c691e19498f58d6
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-02-06 12:04:06 +0000

    Add terraform remote state config for staging-paas

--- terraform/staging-paas/site.tf
+++ terraform/staging-paas/site.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 0.12.0"
+
+  required_providers {
+    cloudfoundry = ">= 0.11.0"
+  }
+
+  backend "s3" {
+    bucket = "govuk-pay-terraform-state"
+    key    = "staging-paas"
+    region = "eu-west-2"
+  }
+}
+
 provider "cloudfoundry" {
   version = "0.11.0"
   api_url = "https://api.cloud.service.gov.uk"
