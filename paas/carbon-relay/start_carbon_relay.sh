f699f6ca596db6922569fece1ce0650ed4a3bf11
danworth <dan.worth@digital.cabinet-office.gov.uk>
2020-03-12 15:44:26 +0000

    PP-6254 Run stunnel as a 'side-car'
    This allows us to change instances, memory and cpu indendently to. CF  should
    alert us and take steps to restart both carbon-relay and its sidecar stunnnel
    should one or the other fail.

--- paas/carbon-relay/start_carbon_relay.sh
+++ paas/carbon-relay/start_carbon_relay.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-erb stunnel.conf.erb > stunnel.conf
 erb carbon-relay-ng.ini.erb > carbon-relay-ng.ini
-
-stunnel stunnel.conf hosted_graphite &
 carbon-relay-ng carbon-relay-ng.ini
-
