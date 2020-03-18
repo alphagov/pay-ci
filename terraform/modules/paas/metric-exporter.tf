9e81788ed8df15872d7ababc29d3d65ff3598076
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-03-18 13:47:36 +0000

    Add metric-exporter terraform app definition

---
+++ terraform/modules/paas/metric-exporter.tf
@@ -0,0 +1,24 @@
+locals {
+  metric_exporter_credentials = lookup(var.credentials, "metric-exporter")
+}
+
+resource "cloudfoundry_app" "metric_exporter" {
+  name    = "metric-exporter"
+  space   = data.cloudfoundry_space.space.id
+  stopped = true
+
+  lifecycle {
+    ignore_changes = [stopped, health_check_type]
+  }
+}
+
+module "metric_exporter_credentials" {
+  source = "../credentials"
+  pay_low_pass_secrets = lookup(local.metric_exporter_credentials, "pay_low_pass_secrets")
+}
+
+resource "cloudfoundry_user_provided_service" "metric_exporter_secret_service" {
+  name        = "metric-exporter-secret-service"
+  space       = data.cloudfoundry_space.space.id
+  credentials = merge(module.metric_exporter_credentials.secrets, lookup(local.metric_exporter_credentials, "static_values"))
+}
