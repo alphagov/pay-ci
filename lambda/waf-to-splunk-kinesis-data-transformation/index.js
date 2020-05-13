94eb99ecc3631fc9a904f3eea1a3e32aab924fb2
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-05-13 09:25:44 +0100

    Log dropped and ok counts from transformation lambda

--- lambda/waf-to-splunk-kinesis-data-transformation/index.js
+++ lambda/waf-to-splunk-kinesis-data-transformation/index.js
@@ -1,10 +1,13 @@
 exports.handler = async (event, context) => {
     /* Process the list of records and transform them */
+    const okCount = 0
+    const droppedCount = 0
     const output = event.records.map((record) => {
         const eventJson = Buffer.from(record.data, 'base64').toString('utf8')
         const event = JSON.parse(eventJson)
 
         if (event.action === 'ALLOW') {
+            droppedCount++
             return {
                 recordId: record.recordId,
                 result: 'Dropped',
@@ -12,6 +15,7 @@ exports.handler = async (event, context) => {
             };
         }
 
+        okCount++
         const data = {
             time: (event.timestamp / 1000),
             host: "lambda",
@@ -27,6 +31,6 @@ exports.handler = async (event, context) => {
             data: Buffer.from(JSON.stringify(data), 'utf8').toString('base64')
         }
     });
-    console.log(`Processing completed. Successful records ${output.length}.`)
+    console.log(`Processing completed. Successful records ${output.length}. Dropped ${droppedCount}. Ok ${okCount}.`)
     return { records: output };
 };
