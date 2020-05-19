e4f3b0602fef768a9e99f48dfc3f0f4ff3b4b016
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-05-19 08:30:33 +0100

    Bind PaaS metric exporter and carbon relay apps to Splunk log service

--- terraform/modules/paas/splunk-log-drain.tf
+++ terraform/modules/paas/splunk-log-drain.tf
@@ -6,6 +6,7 @@ resource "cloudfoundry_service_instance" "splunk_log_service" {
   name            = "splunk-log-service"
   service_plan    = data.cloudfoundry_service.splunk.service_plans["unlimited"]
   space           = data.cloudfoundry_space.space.id
+
   lifecycle {
     prevent_destroy = true
   }
@@ -15,6 +16,7 @@ resource "cloudfoundry_service_instance" "cde_splunk_log_service" {
   name            = "splunk-log-service"
   service_plan    = data.cloudfoundry_service.splunk.service_plans["unlimited"]
   space           = data.cloudfoundry_space.cde_space.id
+
   lifecycle {
     prevent_destroy = true
   }
