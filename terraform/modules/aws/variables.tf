4964c8a946f55bbe94ef8515b3e5cdc4f54b9e2f
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-06-02 09:23:09 +0100

    PP-6234: Add PaaS Ireland public IPs to TF variables
    Public static egress IPs for PaaS Ireland.
    
    These may be used to create AWS resource IP restrictions etc.

--- terraform/modules/aws/variables.tf
+++ terraform/modules/aws/variables.tf
@@ -12,3 +12,8 @@ variable "paas_domain" {
   type        = string
   description = "PaaS domain for Cloudfront origin"
 }
+
+variable "paas_public_ips" {
+  type = list
+  default = [ "52.208.24.161", "52.208.1.143", "52.51.250.21" ]
+}
