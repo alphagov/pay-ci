81543a17044f3a62821d760b3e385df61c777963
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-08-11 16:07:12 +0100

    Create TF state bucket module definition for all environments
    Allows flexibility to define public access block settings.

---
+++ terraform/modules/state-bucket/state-bucket.tf
@@ -0,0 +1,29 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ terraform/modules/state-bucket/variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  type        = string
+  description = "Name of TF state bucket"
+}
