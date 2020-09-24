7b1be29f2b839d0d4b20911ddeb557448f879c7a
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-09-24 13:14:01 +0100

    PP-7128: Bootstrap performance testing AWS account
    Adds Terraform to manage state bucket and IAM roles allowing access to
    performance testing AWS account.
    
    Signed-off-by: Richard Baker <richard.baker@digital.cabinet-office.gov.uk>

---
+++ terraform/performance-account/site.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 0.12"
+
+  # Comment out when bootstrapping
+  backend "s3" {
+    bucket = "govuk-pay-performance-tfstate"
+    key    = "account.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  version = "~> 3.0"
+  region  = "eu-west-1"
+}
+
+module "state_bucket" {
+  source      = "../modules/state-bucket"
+  bucket_name = "govuk-pay-performance-tfstate"
+}
+
+module "cyber_security_audit_role" {
+  source = "git::https://github.com/alphagov/tech-ops//cyber-security/modules/gds_security_audit_role?ref=720885a9769c40942ff30b32179e1fad18f2ca10"
+
+  chain_account_id = "988997429095"
+}
