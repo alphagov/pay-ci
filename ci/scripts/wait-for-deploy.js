c50dd00fd8cd41b580bf93e590e0843d223b9af9
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2021-02-24 14:05:03 +0000

    PP-7801 Make wait-for-deploy script a bit nicer

--- ci/scripts/wait-for-deploy.js
+++ ci/scripts/wait-for-deploy.js
@@ -5,6 +5,8 @@ const ecs = new AWS.ECS()
 const MAX_RETRIES = 120
 const CHECK_INTERVAL = 5000
 const { APP_NAME: appName, TAG: appVersion } = process.env
+let counter = 0
+let deploymentChecker
 
 function describeServices() {
   const params = {
@@ -14,38 +16,38 @@ function describeServices() {
   return ecs.describeServices(params).promise()
 }
 
-async function run() {
-  let uncompletedDeployments
-  let counter = 0
-  const deploymentChecker = setInterval(async () => {
-    counter++
-    if (counter === MAX_RETRIES) {
-      console.log(`Deployment did not complete after ${MAX_RETRIES*CHECK_INTERVAL} seconds.`)
+async function checkDeployment() {
+  counter++
+  if (counter === MAX_RETRIES) {
+    console.log(`Deployment did not complete after ${MAX_RETRIES*CHECK_INTERVAL} seconds.`)
+    process.exitCode = 1
+    clearInterval(deploymentChecker)
+  }
+  const data = await describeServices()
+  const uncompletedDeployments = data.services[0].deployments
+    .filter(deployment => deployment.rolloutState !== 'COMPLETED')
+  if (uncompletedDeployments.length === 0) {
+    console.log('Deployment successful')
+    clearInterval(deploymentChecker)
+  } else {
+    const {taskDefinition, rolloutState, rolloutStateReason} = uncompletedDeployments[0]
+    if (rolloutState === 'FAILED') {
+      console.log(
+        `Deployment failed.
+         Reason ${rolloutStateReason}`
+      )
       process.exitCode = 1
       clearInterval(deploymentChecker)
     }
-    const data = await describeServices()
-    uncompletedDeployments = data.services[0].deployments
-      .filter(deployment => deployment.rolloutState !== 'COMPLETED')
-    if (uncompletedDeployments.length === 0) {
-      console.log('Deployment successful')
-      clearInterval(deploymentChecker)
-    } else {
-      const {taskDefinition, rolloutState, rolloutStateReason} = uncompletedDeployments[0]
-      if (rolloutState === 'FAILED') {
-        console.log(
-          `Deployment failed.
-           Reason ${rolloutStateReason}`
-        )
-        process.exitCode = 1
-        clearInterval(deploymentChecker)
-      }
-      if (rolloutState === 'IN_PROGRESS' && counter === 1) {
-        console.log('Deployment details:')
-        console.table({taskDefinition, deploymentStatus: rolloutState, appVersion})
-      }
-     }
-  }, CHECK_INTERVAL)
+    if (rolloutState === 'IN_PROGRESS' && counter === 1) {
+      console.log('Deployment details:')
+      console.table({taskDefinition, deploymentStatus: rolloutState, appVersion})
+    }
+  }
+}
+
+async function run() {
+  deploymentChecker = setInterval(checkDeployment, CHECK_INTERVAL)
 }
 
 run()
