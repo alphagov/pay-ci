76bf26f5ffa0486a6c5605f934e2664ae1c92ff3
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2021-03-01 17:06:44 +0000

    PP-7834: Check frontend pact compatibility

--- ci/scripts/get-git-sha-for-release-tag.js
+++ ci/scripts/get-git-sha-for-release-tag.js
@@ -6,13 +6,18 @@ const octokit = new Octokit()
 const { TAG: imageTag, APP_NAME: appName} = process.env
 
 async function run() {
-  const gitTag = `alpha_release-${imageTag}`.replace('-release','')
-  const refResult = await octokit.git.getRef({owner: 'alphagov', repo: `pay-${appName}`, ref: `tags/${gitTag}`})
-  const tagSha = refResult.data.object.sha
-  const tagResult = await octokit.git.getTag({owner: 'alphagov', repo: `pay-${appName}`, tag_sha: `${tagSha}`})
 
-  console.log(`git sha: ${tagResult.data.object.sha}`)
-  fs.writeFileSync('git-sha/git-sha', tagResult.data.object.sha)
+  try {
+    const gitTag = `alpha_release-${imageTag}`.replace('-release','')
+    const refResult = await octokit.git.getRef({owner: 'alphagov', repo: `pay-${appName}`, ref: `tags/${gitTag}`})
+    const tagSha = refResult.data.object.sha
+    const tagResult = await octokit.git.getTag({owner: 'alphagov', repo: `pay-${appName}`, tag_sha: `${tagSha}`})
+    console.log(`git sha: ${tagResult.data.object.sha}`)
+    fs.writeFileSync('git-sha/git-sha', tagResult.data.object.sha)
+  } catch (err) {
+    console.log(`An error occurred: ${err}`)
+    process.exitCode = 1
+  }
 }
 
 run()
