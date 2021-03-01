6ecabcbae6b450be8190d48cc62cc0f9eb21ae0f
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2021-03-01 16:43:15 +0000

    PP-7834: Tag connector pacticipant with 'test-fargate'

--- ci/scripts/get-git-sha-for-release-tag.js
+++ ci/scripts/get-git-sha-for-release-tag.js
@@ -9,7 +9,7 @@ async function run() {
   const gitTag = `alpha_release-${imageTag}`.replace('-release','')
   const refResult = await octokit.git.getRef({owner: 'alphagov', repo: `pay-${appName}`, ref: `tags/${gitTag}`})
   const tagSha = refResult.data.object.sha
-  const tagResult = await octokit.git.getTag({owner: 'alphagov', repo: 'pay-frontend', tag_sha: `${tagSha}`})
+  const tagResult = await octokit.git.getTag({owner: 'alphagov', repo: `pay-${appName}`, tag_sha: `${tagSha}`})
 
   console.log(`git sha: ${tagResult.data.object.sha}`)
   fs.writeFileSync('git-sha/git-sha', tagResult.data.object.sha)
