8ebbf952d911e9c21bfae3cf622e2b7297984c7e
danworth <dan.worth@digital.cabinet-office.gov.uk>
2021-03-15 15:38:24 +0000

    PP-7881 Run lint on existing Node.js scripts

--- ci/scripts/get-git-sha-for-release-tag.js
+++ ci/scripts/get-git-sha-for-release-tag.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const { Octokit } = require('@octokit/rest');
+const fs = require('fs')
+const { Octokit } = require('@octokit/rest')
 const octokit = new Octokit()
-const { TAG: imageTag, APP_NAME: appName} = process.env
+const { TAG: imageTag, APP_NAME: appName } = process.env
 
-async function run() {
-
+async function run () {
   try {
-    const gitTag = `alpha_release-${imageTag}`.replace('-release','')
-    const refResult = await octokit.git.getRef({owner: 'alphagov', repo: `pay-${appName}`, ref: `tags/${gitTag}`})
+    const gitTag = `alpha_release-${imageTag}`.replace('-release', '')
+    const refResult = await octokit.git.getRef({ owner: 'alphagov', repo: `pay-${appName}`, ref: `tags/${gitTag}` })
     const tagSha = refResult.data.object.sha
-    const tagResult = await octokit.git.getTag({owner: 'alphagov', repo: `pay-${appName}`, tag_sha: `${tagSha}`})
+    const tagResult = await octokit.git.getTag({ owner: 'alphagov', repo: `pay-${appName}`, tag_sha: `${tagSha}` })
     console.log(`git sha: ${tagResult.data.object.sha}`)
     fs.writeFileSync('git-sha/git-sha', tagResult.data.object.sha)
   } catch (err) {
