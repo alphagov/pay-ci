737681e59f112b618470b3393ef1dc2912358a27
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2021-03-24 17:02:17 +0000

    PP-7908 Check release versions before deployment
    In our current Jenkins pipelines we have a check that
    stops devs accidentally deploying an old version of an
    app. This `check-release-versions` task achieves same thing
    on Concourse - it checks the version to be deployed of each
    container in a task definition, and makes sure it is the
    same as or newer than currently deployed version.
    
    There is no mechanism to override this at the moment -
    if override is needed devs will need to deploy the thing
    manually from laptop.

---
+++ ci/scripts/check-release-versions.js
@@ -0,0 +1,71 @@
+const AWS = require('aws-sdk')
+const ecs = new AWS.ECS()
+
+async function getService (appName, clusterName) {
+  try {
+    const params = {
+      services: [appName],
+      cluster: clusterName
+    }
+    const describeServices = await ecs.describeServices(params).promise()
+    return describeServices.services.find(service => service.status === 'ACTIVE')
+  } catch (err) {
+    throw new Error(`Error fetching service: ${err.message}`)
+  }
+}
+
+async function getTaskDefinitionDetails (taskDefinitionName) {
+  try {
+    const { taskDefinition } = await ecs.describeTaskDefinition({ taskDefinition: taskDefinitionName }).promise()
+    return taskDefinition
+  } catch (err) {
+    throw new Error(`Error fetching task definition details: ${err.message}`)
+  }
+}
+
+function checkReleaseVersion (containerName, tagToBeDeployed, currentContainerDefinitions) {
+  const currentImage = currentContainerDefinitions.find(container => container.name === containerName).image
+  const currentRelease = Number(currentImage.split(':')[1].split('-')[0])
+  const releaseToBeDeployed = Number(tagToBeDeployed.split('-')[0])
+  if (releaseToBeDeployed < currentRelease) {
+    throw new Error(`
+        You are trying to deploy release  ${releaseToBeDeployed} of ${containerName} 
+        which is older than the current release ${currentRelease}. Bailing out.
+        If you need to deploy this version please deploy manually using terraform.`)
+  }
+}
+
+async function run () {
+  const {
+    CLUSTER_NAME,
+    APP_NAME,
+    APPLICATION_IMAGE_TAG,
+    TELEGRAF_IMAGE_TAG,
+    NGINX_IMAGE_TAG,
+    NGINX_FORWARD_PROXY_IMAGE_TAG
+  } = process.env
+
+  try {
+    const service = await getService(APP_NAME, CLUSTER_NAME)
+    if (!service) {
+      throw new Error(`failed to find active service for ${APP_NAME}`)
+    }
+
+    const { containerDefinitions } = await getTaskDefinitionDetails(service.taskDefinition)
+    if (!containerDefinitions) {
+      throw new Error('failed to get task definition details')
+    }
+
+    checkReleaseVersion(APP_NAME, APPLICATION_IMAGE_TAG, containerDefinitions)
+    checkReleaseVersion('telegraf', TELEGRAF_IMAGE_TAG, containerDefinitions)
+    checkReleaseVersion('nginx', NGINX_IMAGE_TAG, containerDefinitions)
+    if (NGINX_FORWARD_PROXY_IMAGE_TAG) {
+      checkReleaseVersion('nginx-forward-proxy', NGINX_FORWARD_PROXY_IMAGE_TAG, containerDefinitions)
+    }
+  } catch (err) {
+    console.log(err.message)
+    process.exit(1)
+  }
+}
+
+run()
