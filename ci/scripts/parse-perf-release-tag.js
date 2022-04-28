6458a8e8c18e936fd0025954e69ce735ca4f9776
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2022-04-28 16:14:11 +0100

    [to be squashed]: use string replace

--- ci/scripts/parse-perf-release-tag.js
+++ ci/scripts/parse-perf-release-tag.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const sidecars = ['egress','stunnel','carbon-relay']
 
 async function run () {
   try {
@@ -11,14 +10,9 @@ async function run () {
     }
 
     const tag = fs.readFileSync('ecr-repo/tag', 'utf8')
-    const releaseNum = tag.split('-')[0]
-    const maybeSidecar = tag.split('-')[1]
-    if (sidecars.includes(maybeSidecar)) {
-      fs.writeFileSync('parse-perf-release-tag/tag', `${releaseNum}-${maybeSidecar}-perf`)
-    } else {
-      fs.writeFileSync('parse-perf-release-tag/tag', `${releaseNum}-perf`)
-    }
-    console.log('Contents of parse-perf-release-tag/tag: ' + fs.readFileSync('parse-perf-release-tag/tag', 'utf8'))
+    fs.writeFileSync('parse-perf-release-tag/tag', tag.replace('release', 'perf'))
+
+    console.log('Resulting tag: ' + fs.readFileSync('parse-perf-release-tag/tag', 'utf8'))
   } catch (err) {
     console.log(`An error occurred: ${err}`)
     process.exitCode = 1
