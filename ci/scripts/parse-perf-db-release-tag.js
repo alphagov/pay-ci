582e0d73aebb2e179683330be260ce37266afe13
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2022-05-06 14:49:57 +0100

    [to be squashed]: parse-perf-db-release-tag

--- ci/scripts/parse-perf-db-release-tag.js
+++ ci/scripts/parse-perf-db-release-tag.js
@@ -4,15 +4,15 @@ const fs = require('fs')
 
 async function run () {
   try {
-    const dir = 'parse-perf-release-tag'
+    const dir = 'parse-perf-db-release-tag'
     if (!fs.existsSync(dir)){
       fs.mkdirSync(dir);
     }
 
     const tag = fs.readFileSync('ecr-repo/tag', 'utf8')
-    fs.writeFileSync('parse-perf-release-tag/tag', tag.replace('release', 'perf'))
+    fs.writeFileSync(`${dir}/tag`, tag.replace('release', 'perf-db'))
 
-    console.log('Resulting tag: ' + fs.readFileSync('parse-perf-release-tag/tag', 'utf8'))
+    console.log('Resulting tag: ' + fs.readFileSync(`${dir}/tag`, 'utf8'))
   } catch (err) {
     console.log(`An error occurred: ${err}`)
     process.exitCode = 1
