8bfa54754ccb24e53aa4e7fc2debb8e090e37553
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2023-02-15 17:28:24 +0000

    PP-10667 added semver github action
    Added a new action to determine the correct semantic version to tag a new release with given two possible values
    - the action will patch the semantic version by 1 if there is an existing release value that is greater than the source version
    - the action will use the source value if is higher than the latest release version
    - the action will patch the semantic version by 1 if the source and existing release versions are the same

---
+++ actions/next-semver/index.ts
@@ -0,0 +1,54 @@
+// noinspection DuplicatedCode
+
+const core = require('@actions/core')
+
+// this script expects two values, they must be semantic versions conforming to the MAJOR.MINOR.PATCH convention
+try {
+  const packageVersion: string = core.getInput('package_version')
+  const currentReleaseVersion: string = core.getInput('release_version')
+
+  const packageVersionParts: string[] = packageVersion.split(".")
+  const currentReleaseVersionParts: string[] = currentReleaseVersion.split(".")
+
+  if (arrayValuesAreNumbers([packageVersionParts, currentReleaseVersionParts])) {
+    let parts: string []
+    switch (compareSemver(packageVersion, currentReleaseVersion)) {
+      case 0:
+        //  0: versions are equal, patch package version
+        parts = incrementPatchValue(packageVersionParts)
+        break
+      case 1:
+        //  1: package version is greater than current release version, use package version
+        parts = packageVersionParts
+        break
+      default:
+        //  -1: current release version is greater than package version and all other cases, patch current release version
+        parts = incrementPatchValue(currentReleaseVersionParts)
+        break
+    }
+    const newReleaseVersion: string = parts.join('.')
+    console.log(`New release version: ${newReleaseVersion}`)
+    core.setOutput('version', newReleaseVersion)
+  } else {
+    core.setFailed(`input values must be semantic (MAJOR.MINOR.PATCH) [values provided: ${packageVersion}, ${currentReleaseVersion}]`)
+  }
+} catch (error: any) {
+  core.setFailed(error.message)
+}
+
+function arrayValuesAreNumbers(arrays: string[][]): boolean {
+  return arrays.every(arr => arr.every(element => !isNaN(Number(element))))
+}
+
+function compareSemver(a: string, b: string): number {
+  return a.localeCompare(b, undefined, {numeric: true, sensitivity: 'base'})
+}
+
+function incrementPatchValue(parts: string[]): string[] {
+  let patchValue = parseInt(parts[parts.length - 1]!, 10)
+  patchValue++
+  parts[parts.length - 1] = patchValue.toString()
+  return parts
+}
+
+
