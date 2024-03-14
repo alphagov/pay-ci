cf5e29d2c253b5bc732c6c319f1ac345f9c56f1a
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2024-03-14 08:41:18 +0000

    PP-12420 Convert deploy-to-perf pipeline to pkl

---
+++ ci/scripts/deploy-notifications.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd pay-infra/provisioning/terraform/deployments/${ACCOUNT}/${ENVIRONMENT}/microservices_v2/notifications
+
+terraform init
+
+terraform apply \
+  -var notifications_image_tag=${APPLICATION_IMAGE_TAG} \
+  -auto-approve
