358f62e8615b561b8137507a7298a11ccfc492e0
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2024-03-18 13:54:23 +0000

    PP-12420 Fix deploy-notifications to perf task

--- ci/scripts/deploy-notifications.sh
+++ ci/scripts/deploy-notifications.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-
+#!/bin/ash
+# shellcheck shell=dash
 set -euo pipefail
 
-cd pay-infra/provisioning/terraform/deployments/${ACCOUNT}/${ENVIRONMENT}/microservices_v2/notifications
+cd "pay-infra/provisioning/terraform/deployments/${ACCOUNT}/${ENVIRONMENT}/microservices_v2/notifications"
 
 terraform init
 
 terraform apply \
-  -var notifications_image_tag=${APPLICATION_IMAGE_TAG} \
+  -var notifications_image_tag="${APPLICATION_IMAGE_TAG}" \
   -auto-approve
