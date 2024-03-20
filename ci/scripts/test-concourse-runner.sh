6aa9f4f4c9c1ca66af66da8dc22dcfc13d3aba73
Kerr Rainey <kerr.rainey@digital.cabinet-office.gov.uk>
2024-03-20 12:40:35 +0000

    PP-12424: Extract test-concourse-runner script
    Extract the script executed in the `test-concourse-runner`, making it it's own file and referencing it.

---
+++ ci/scripts/test-concourse-runner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eo pipefail
+
+source /docker-helpers.sh
+
+start_docker
+
+docker ps -a
+
+cd app-repo
+
+export MAVEN_HOME=/usr/lib/mvn
+export PATH=$MAVEN_HOME/bin:$PATH
+export MAVEN_REPO="$PWD/.m2"
+
+cat <<'EOF' >settings.xml
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+https://maven.apache.org/xsd/settings-1.0.0.xsd">
+<localRepository>${env.MAVEN_REPO}</localRepository>
+</settings>
+EOF
+
+mvn --global-settings settings.xml clean verify
+
+stop_docker
