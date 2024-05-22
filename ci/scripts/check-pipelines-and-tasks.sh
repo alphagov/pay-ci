2300420c815fb1b7c6f614178dee971ddda8296e
rfisher-gds <141360735+rfisher-gds@users.noreply.github.com>
2024-05-22 11:02:13 +0100

    PP-12582-migrate-pr-pipeline-to-pkl (#1212)
    * recreate pr-ci pipeline with pkl
    * break out shared e2e-helpers functions into library
    * break out embedded scripts
    * modify names to be consistent with other pipelines

---
+++ ci/scripts/check-pipelines-and-tasks.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+apk add git shellcheck
+go install github.com/alphagov/paas-cf/tools/pipecleaner@latest
+
+cd /tmp/
+
+echo "c7d331052a6bf552b017adf5288b8e162346157c  fly-7.6.0-linux-amd64.tgz" > fly-7.6.0-linux-amd64.tgz.sha1
+wget -c https://github.com/concourse/concourse/releases/download/v7.6.0/fly-7.6.0-linux-amd64.tgz -O fly-7.6.0-linux-amd64.tgz
+sha1sum -c fly-7.6.0-linux-amd64.tgz.sha1
+tar -O -zxf fly-7.6.0-linux-amd64.tgz > /usr/local/bin/fly
+chmod u+x /usr/local/bin/fly
+
+cd -
+
+pipecleaner --rubocop=false ci/tasks/*.yml
+
+find ci/pipelines -name '*.yml' | while read -r PIPELINE; do
+  echo "Validating: $PIPELINE"
+  fly validate-pipeline -c "$PIPELINE"
+  echo
+done
