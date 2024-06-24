ccb88aae4dc501f187e61fa7fbf4a05c6c663ba7
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2024-06-24 10:11:06 +0100

    PP-12783 Add editorconfig and format assume-role script (#1236)

--- ci/scripts/assume-role.js
+++ ci/scripts/assume-role.js
@@ -7,31 +7,31 @@ const { STSClient, AssumeRoleCommand } = require('@aws-sdk/client-sts')
 const stsClient = new STSClient({ region: 'eu-west-1' });
 
 const run = async function run() {
-    const roleParams = {
-        RoleArn: process.env.AWS_ROLE_ARN,
-        RoleSessionName: process.env.AWS_ROLE_SESSION_NAME,
-    }
+  const roleParams = {
+    RoleArn: process.env.AWS_ROLE_ARN,
+    RoleSessionName: process.env.AWS_ROLE_SESSION_NAME,
+  }
 
-    if (process.env.AWS_ROLE_DURATION) {
-        roleParams['DurationSeconds'] = process.env.AWS_ROLE_DURATION
-    }
+  if (process.env.AWS_ROLE_DURATION) {
+    roleParams['DurationSeconds'] = process.env.AWS_ROLE_DURATION
+  }
 
-    try {
-        const command = new AssumeRoleCommand(roleParams)
-        const assumeRoleResponse = await stsClient.send(command);
+  try {
+    const command = new AssumeRoleCommand(roleParams)
+    const assumeRoleResponse = await stsClient.send(command);
 
-        const tempCreds = assumeRoleResponse.Credentials
+    const tempCreds = assumeRoleResponse.Credentials
 
-        fs.writeFileSync('assume-role/assume-role.json', JSON.stringify({
-            AWS_ACCESS_KEY_ID: tempCreds.AccessKeyId,
-            AWS_SECRET_ACCESS_KEY: tempCreds.SecretAccessKey,
-            AWS_SESSION_TOKEN: tempCreds.SessionToken
-        }))
+    fs.writeFileSync('assume-role/assume-role.json', JSON.stringify({
+      AWS_ACCESS_KEY_ID: tempCreds.AccessKeyId,
+      AWS_SECRET_ACCESS_KEY: tempCreds.SecretAccessKey,
+      AWS_SESSION_TOKEN: tempCreds.SessionToken
+    }))
 
-    } catch (err) {
-        console.error('Failed to assume role: ', err.message)
-        process.exit(1)
-    }
+  } catch (err) {
+    console.error('Failed to assume role: ', err.message)
+    process.exit(1)
+  }
 
 }
 
