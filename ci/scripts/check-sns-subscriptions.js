7c59398caff15a0ccd834a7a62911d2483ff4d1c
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2024-06-25 09:16:06 +0100

    PP-12783 Update infra-detector scripts to AWS SDK v3 (#1241)

--- ci/scripts/check-sns-subscriptions.js
+++ ci/scripts/check-sns-subscriptions.js
@@ -9,21 +9,22 @@
  * not considered a secret, but is deemed sufficiently sensitive to not be in a public repo; or
  * it may be a second ARN. If this is the case, that ARN is presumed to point to an encrypted
  * value in Parameter Store, from where it is retrieved, and its plaintext value compared to the
- * thing which subscribes to the original topic. These encrypted values are probably Pagerduty 
+ * thing which subscribes to the original topic. These encrypted values are probably Pagerduty
  * URIs.
  *
  * If a subscriber does not match its expected value, the topic ARN is printed to the console. As
  * subscriber values can be secrets, no more information is logged.
  *
- * In the test environment, it is okay for a topic to have no subscribers, but this is not 
+ * In the test environment, it is okay for a topic to have no subscribers, but this is not
  * acceptable elsewhere. If you want to ignore empty subscriptions, set OK_TO_BE_UNSUBSCRIBED in the
  * script's execution environment.
  *
  */
 
 const fs = require('fs')
-const AWS = require('aws-sdk')
-require('aws-sdk/lib/maintenance_mode_message').suppress = true
+
+const { SNSClient, ListSubscriptionsByTopicCommand, ListTopicsCommand } = require('@aws-sdk/client-sns')
+const { SSMClient, GetParameterCommand } = require('@aws-sdk/client-ssm')
 
 var TRUTH_FILE = 'pay-infra/provisioning/config/sns_topic_truth.json'
 
@@ -35,7 +36,7 @@ var regions = process.env.REGIONS
 var errors = 0
 var validated = 0
 
-function loadTruth(file) {
+function loadTruth (file) {
   if (!fs.existsSync(file)) {
     console.error(`No truth file at '${file}'`)
     process.exit(2)
@@ -48,12 +49,12 @@ function loadTruth(file) {
   }
 }
 
-function logError(msg) {
+function logError (msg) {
   errors += 1
   console.warn(`ERROR: ${msg}`)
 }
 
-function compare(topicArn, expected, actual) {
+function compare (topicArn, expected, actual) {
   if (expected === actual) {
     validated += 1
   } else if (expected == null) {
@@ -63,70 +64,70 @@ function compare(topicArn, expected, actual) {
   }
 }
 
-function getParam(paramArn) {
+async function getParam (paramArn) {
   try {
     const region = paramArn.split(':')[3]
-    const client = new AWS.SSM({ region: region })
-    return client.getParameter({ Name: paramArn, WithDecryption: true }).promise()
-      .then(param => param.Parameter.Value)
-      .catch(err => {
-        logError(err.code)
-        throw err
-      })
+    const ssmClient = new SSMClient({ region: region })
+
+    const command = new GetParameterCommand({ Name: paramArn, WithDecryption: true })
+    const response = await ssmClient.send(command)
+
+    return response.Parameter.Value
   } catch (err) {
-    logError(err.code)
+    throw err
   }
 }
 
 // HTTPS endpoints are Pagerduty URIs, which have a token embedded in them.
 // Therefore they are sensitive, and have to be fetched from Parameter Store.
-async function validateSubscriptionHttps(topicArn, endpoint) {
+async function validateSubscriptionHttps (topicArn, endpoint) {
   const expected = TRUTH[topicArn]
   if (expected) {
-    getParam(expected)
-      .then(secret => {
-        compare(topicArn, endpoint, secret)
-      })
-      .catch(err => {
-        logError(err)
-      })
+    try {
+      const secretParam = await getParam(expected)
+      compare(topicArn, endpoint, secretParam)
+    } catch (err) {
+      logError(err.name)
+    }
   }
 }
 
-function validateSubscriptionGeneric(topicArn, endpoint) {
+function validateSubscriptionGeneric (topicArn, endpoint) {
   compare(topicArn, TRUTH[topicArn], endpoint)
 }
 
-function validateSubscriptions(subscriptions) {
+async function validateSubscriptions (subscriptions) {
   for (const subscription of subscriptions) {
     const endpoint = subscription.Endpoint.trim()
     if (subscription.Protocol === 'email' || subscription.Protocol === 'sqs') {
       validateSubscriptionGeneric(subscription.TopicArn, endpoint)
     } else if (subscription.Protocol === 'https') {
-      validateSubscriptionHttps(subscription.TopicArn, endpoint)
+      await validateSubscriptionHttps(subscription.TopicArn, endpoint)
     } else {
       logError(`Unknown protocol '${subscription.Protocol}' for ${subscription.TopicArn}`)
     }
   }
 }
 
-async function processTopics(client, data) {
+async function processTopics (snsClient, data) {
   let subsToValidate = []
 
   const promises = data.Topics.map(async (topic) => {
     try {
-      const subs = await client.listSubscriptionsByTopic(topic).promise()
+      const command = new ListSubscriptionsByTopicCommand(topic)
+      const subs = await snsClient.send(command)
+
       subsToValidate = subsToValidate.concat(subs.Subscriptions)
       if (!process.env.OK_TO_BE_UNSUBSCRIBED && subs.Subscriptions.length === 0) {
         logError(`${topic.TopicArn} has no subscriptions`)
       }
     } catch (err) {
-      logError(`Problem fetching subscriptions for topic ${topicArn}: ${err}`)
+      logError(`Problem fetching subscriptions for topic ${topic.TopicArn}: ${err}`)
     }
   })
 
   await Promise.all(promises)
-  validateSubscriptions(subsToValidate)
+  await validateSubscriptions(subsToValidate)
 }
 
 const TRUTH = loadTruth(TRUTH_FILE)
@@ -138,12 +139,15 @@ if (!regions) {
 
 regions = regions.split(/\s+/)
 
-async function run(regions) {
+async function run (regions) {
   const promises = regions.map(async (region) => {
-    const client = new AWS.SNS({ region: region })
     try {
-      const topics = await client.listTopics().promise()
-      await processTopics(client, topics)
+      const snsClient = new SNSClient({ region: region })
+      const command = new ListTopicsCommand({})
+
+      const topics = await snsClient.send(command)
+
+      await processTopics(snsClient, topics)
     } catch (err) {
       logError(`Problem fetching topics: ${err}`)
     }
