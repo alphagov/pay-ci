b348d3f50392a0025c22967d7736d92475480111
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2024-06-27 18:54:04 +0100

    PP-12827: Parameterise deadlock timeout

--- ci/scripts/lock-pools/detect-potential-deadlock.sh
+++ ci/scripts/lock-pools/detect-potential-deadlock.sh
@@ -3,6 +3,11 @@
 
 set -euo pipefail
 
+if [ -z "${POTENTIAL_DEADLOCK_TIMEOUT_IN_MINUTES:-}" ]; then
+  echo "Error: The env var POTENTIAL_DEADLOCK_TIMEOUT_IN_MINUTES is not set."
+  exit 1
+fi
+
 LOCK_STATUS=$(cat lock-status/status)
 DATE=$(cat lock-status/date)
 LOCK_CLAIMED_TIMESTAMP=$(cat lock-status/timestamp)
@@ -14,20 +19,21 @@ fi
 
 CURRENT_TIMESTAMP=$(date +%s)
 
-POTENTIAL_DEADLOCK_TIMEOUT_IN_SECONDS=900 # 15 minutes
+POTENTIAL_DEADLOCK_TIMEOUT_IN_SECONDS=$((POTENTIAL_DEADLOCK_TIMEOUT_IN_MINUTES * 60))
 POTENTIAL_DEADLOCK_IF_CLAIMED_BEFORE=$((CURRENT_TIMESTAMP - POTENTIAL_DEADLOCK_TIMEOUT_IN_SECONDS))
+IGNORE_POTENTIAL_DEADLOCK_BEFORE_IN_MINUTES=$((POTENTIAL_DEADLOCK_TIMEOUT_IN_MINUTES * 2))
 IGNORE_POTENTIAL_DEADLOCK_BEFORE=$((CURRENT_TIMESTAMP - (POTENTIAL_DEADLOCK_TIMEOUT_IN_SECONDS * 2)))
 
 if [ "$LOCK_CLAIMED_TIMESTAMP" -gt "$POTENTIAL_DEADLOCK_IF_CLAIMED_BEFORE" ]; then
-  echo "Lock was claimed at $DATE, but this is less than 15 minutes ago, nothing to do."
+  echo "Lock was claimed at $DATE, but this is less than $POTENTIAL_DEADLOCK_TIMEOUT_IN_MINUTES minutes ago, nothing to do."
   exit 0
 fi
 
 if [ "$LOCK_CLAIMED_TIMESTAMP" -lt "$IGNORE_POTENTIAL_DEADLOCK_BEFORE" ]; then
-  echo "Lock was claimed at $DATE, this is more than 30 minutes ago, ignoring in the knowledge the auto-deadlock resolution will happen"
+  echo "Lock was claimed at $DATE, this is more than $IGNORE_POTENTIAL_DEADLOCK_BEFORE_IN_MINUTES minutes ago, ignoring in the knowledge the auto-deadlock resolution will happen"
   exit 0
 fi
 
-echo "Lock was claimed at $DATE, which is more than 15 minutes ago"
+echo "Lock was claimed at $DATE, which is more than $POTENTIAL_DEADLOCK_TIMEOUT_IN_MINUTES minutes ago"
 echo "POTENTIAL DEADLOCK DETECTED"
 exit 1
