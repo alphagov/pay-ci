cce9dd9ee9e483835b8cb9700adb3784a6722dfe
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2024-06-27 12:41:38 +0100

    PP-12827: Fix report lock status script

--- ci/scripts/lock-pools/report-lock-status.sh
+++ ci/scripts/lock-pools/report-lock-status.sh
@@ -13,29 +13,24 @@ if [ ! -d "$LOCK_NAME" ]; then
   exit 1
 fi
 
-cd "$LOCK_NAME" >> /dev/null 2>&1
-
 print_and_write_lock_status() {
   # Args:
   #  $1: The status of the lock
-  LOCK_STATUS="$1"
-  
-  # FIXME: REMOVE THIS
-  echo "Full commit message":
-  git log -n 1 --date raw -n 1 "${LOCK_STATUS}/${LOCK_NAME}-lock"
-  echo
+  local LOCK_STATUS="$1"
 
-  COMMIT_DATE=$(git log -n 1 "${LOCK_STATUS}/${LOCK_NAME}-lock" | grep "^Date:" | sed -E 's/^Date:\s+//')
-  COMMIT_TIMESTAMP=$(git log --date raw -n 1 "${LOCK_STATUS}/${LOCK_NAME}-lock" | grep "^Date:" | sed -E 's/^Date:\s+([0-9]+) .*/\1/')
-  COMMIT_MESSAGE=$(git log -n 1 "${LOCK_STATUS}/${LOCK_NAME}-lock" | tail -n 1)
+  local LOCK_FILE_PATH="${LOCK_NAME}/${LOCK_STATUS}/${LOCK_NAME}-lock"
+  
+  local COMMIT_DATE=$(git log -n 1 "$LOCK_FILE_PATH" | grep "^Date:" | sed -E 's/^Date:\s+//')
+  local COMMIT_TIMESTAMP=$(git log --date raw -n 1 "$LOCK_FILE_PATH" | grep "^Date:" | sed -E 's/^Date:\s+([0-9]+) .*/\1/')
+  local COMMIT_MESSAGE=$(git log -n 1 "$LOCK_FILE_PATH" | tail -n 1 | sed -E 's/^\s+//')
 
   # Commit message format:
   # <team>/<pipeline>/<job> build <build_number> <claiming|unclaiming>: <lock_name>-lock
-  FULL_CLAIMANT_INFO=$(echo "$COMMIT_MESSAGE" | cut -f 1 -d " ")
-  CONCOURSE_TEAM=$(echo "$FULL_CLAIMANT_INFO" | cut -f 1 -d "/")
-  CONCOURSE_PIPELINE=$(echo "$FULL_CLAIMANT_INFO" | cut -f 2 -d "/")
-  CONCOURSE_JOB=$(echo "$FULL_CLAIMANT_INFO" | cut -f 3 -d "/")
-  BUILD_NUMBER=$(echo "$COMMIT_MESSAGE" | sed -E 's/.* build ([0-9]+) (un)?claiming: .*/\1/')
+  local FULL_CLAIMANT_INFO=$(echo "$COMMIT_MESSAGE" | cut -f 1 -d " ")
+  local CONCOURSE_TEAM=$(echo "$FULL_CLAIMANT_INFO" | cut -f 1 -d "/")
+  local CONCOURSE_PIPELINE=$(echo "$FULL_CLAIMANT_INFO" | cut -f 2 -d "/")
+  local CONCOURSE_JOB=$(echo "$FULL_CLAIMANT_INFO" | cut -f 3 -d "/")
+  local BUILD_NUMBER=$(echo "$COMMIT_MESSAGE" | sed -E 's/.* build ([0-9]+) (un)?claiming: .*/\1/')
 
   echo
   if [ "$LOCK_STATUS" = "claimed" ]; then
@@ -48,21 +43,21 @@ print_and_write_lock_status() {
   echo "           Job: $CONCOURSE_JOB"
   echo "  Build Number: $BUILD_NUMBER"
   echo "          Date: $COMMIT_DATE"
+  echo "           URL: https://pay-cd.deploy.payments.service.gov.uk/teams/$CONCOURSE_TEAM/pipelines/$CONCOURSE_PIPELINE/jobs/$CONCOURSE_JOB/builds/$BUILD_NUMBER"
   echo
 
-  cd ../lock-status
-  echo "claimed" > status
-  echo "$CONCOURSE_TEAM" > team
-  echo "$CONCOURSE_PIPELINE" > pipeline
-  echo "$CONCOURSE_JOB" > job
-  echo "$BUILD_NUMBER" > build-number
-  echo "$COMMIT_DATE" > "date"
-  echo "$COMMIT_TIMESTAMP" > timestamp
-}
+  echo "claimed" > ../lock-status/status
+  echo "$CONCOURSE_TEAM" > ../lock-status/team
+  echo "$CONCOURSE_PIPELINE" > ../lock-status/pipeline
+  echo "$CONCOURSE_JOB" > ../lock-status/job
+  echo "$BUILD_NUMBER" > ../lock-status/build-number
+  echo "$COMMIT_DATE" > ../lock-status/date
+  echo "$COMMIT_TIMESTAMP" > ../lock-status/timestamp
+} >&2
 
-if [ -f "unclaimed/${LOCK_NAME}-lock" ]; then
+if [ -f "${LOCK_NAME}/unclaimed/${LOCK_NAME}-lock" ]; then
   print_and_write_lock_status "unclaimed"
-elif [ -f "claimed/${LOCK_NAME}-lock" ]; then
+elif [ -f "${LOCK_NAME}claimed/${LOCK_NAME}-lock" ]; then
   print_and_write_lock_status "claimed"
 else
   echo "Error! Couldn't find lock file '${LOCK_NAME}-lock' in either claimed or unclaimed directories"
