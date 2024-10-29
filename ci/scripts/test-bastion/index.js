36232a531355440b87efd6c39c631184f8494e9c
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2024-10-29 14:31:28 +0000

    PP-13142 Bastion E2E test - check DB connection and bastion status
    - For the bastion E2E tests
       - checks DB connection after opening the tunnel
       - and checks bastion task is shut after disconnecting the session

--- ci/scripts/test-bastion/index.js
+++ ci/scripts/test-bastion/index.js
@@ -3,14 +3,24 @@ const {
   RegisterTaskDefinitionCommand,
   DeleteTaskDefinitionsCommand,
   DeregisterTaskDefinitionCommand,
+  ListTasksCommand,
   ListTaskDefinitionsCommand
 } = require('@aws-sdk/client-ecs')
 
+const {
+  SSMClient,
+  GetParametersCommand
+} = require('@aws-sdk/client-ssm')
+
 const { spawn, exec } = require('child_process')
+const { Client } = require('pg')
 
-const client = new ECSClient({ region: 'eu-west-1' })
+const ecsClient = new ECSClient({ region: 'eu-west-1' })
+const ssmClient = new SSMClient({ region: 'eu-west-1' })
 
-let tunnelSuccessful = false, dbConnectionSuccessful = false, sessionId
+let tunnelSuccessful = false, dbConnectionSuccessful = false, bastionShutdown = false
+let familyName, sessionId
+let tunnelCommand
 
 async function createTaskDefinition (imageTag) {
   const origTaskDefinitionFamily = {
@@ -19,7 +29,7 @@ async function createTaskDefinition (imageTag) {
 
   try {
     const command = new DescribeTaskDefinitionCommand(origTaskDefinitionFamily)
-    const response = await client.send(command)
+    const response = await ecsClient.send(command)
 
     let newTaskDef = response.taskDefinition
     newTaskDef.containerDefinitions[0].image = `${process.env.ECR_REPO}:${imageTag}`
@@ -29,7 +39,7 @@ async function createTaskDefinition (imageTag) {
     debug(JSON.stringify(newTaskDef))
 
     const registerTaskDefcommand = new RegisterTaskDefinitionCommand(newTaskDef)
-    let newTaskDefResponse = await client.send(registerTaskDefcommand);
+    let newTaskDefResponse = await ecsClient.send(registerTaskDefcommand)
 
     debug('new task definition below: \n')
     debug(JSON.stringify(newTaskDefResponse))
@@ -50,7 +60,7 @@ async function deleteTaskDefinition (familyName) {
       status: "ACTIVE"
     }
     const listTaskDefinitionsCommand = new ListTaskDefinitionsCommand(listTaskDefsInput)
-    const listTaskDefResponse = await client.send(listTaskDefinitionsCommand)
+    const listTaskDefResponse = await ecsClient.send(listTaskDefinitionsCommand)
 
     for (const arn of listTaskDefResponse.taskDefinitionArns) {
       const revision = arn.substring(arn.lastIndexOf(":") + 1)
@@ -59,8 +69,8 @@ async function deleteTaskDefinition (familyName) {
       const inputDeRegisterTaskDefinition = {
         taskDefinition: `${familyName}:${revision}`
       }
-      const deregisterTaskDefinitionCommand = new DeregisterTaskDefinitionCommand(inputDeRegisterTaskDefinition);
-      await client.send(deregisterTaskDefinitionCommand)
+      const deregisterTaskDefinitionCommand = new DeregisterTaskDefinitionCommand(inputDeRegisterTaskDefinition)
+      await ecsClient.send(deregisterTaskDefinitionCommand)
     }
 
     // delete task definition
@@ -68,7 +78,7 @@ async function deleteTaskDefinition (familyName) {
       taskDefinitions: [familyName]
     }
     const deleteTaskDefinitionsCommand = new DeleteTaskDefinitionsCommand(inputDeleteTaskDefinition)
-    await client.send(deleteTaskDefinitionsCommand)
+    await ecsClient.send(deleteTaskDefinitionsCommand)
 
     log(`Deleted task definition - ${familyName}`)
   } catch (e) {
@@ -80,7 +90,7 @@ async function deleteTaskDefinition (familyName) {
 async function buildPayCLI () {
   return new Promise((resolve, reject) => {
     log('.....Building Pay CLI..... \n')
-    exec('npm install && npm run build', { cwd: getPayCliPath(), timeout: 20000 },
+    exec('npm install && npm run build', { cwd: getPayCliPath(), timeout: 40000 },
       (error, stdout, stderr) => {
         if (error) {
           log(`Error building Pay CLI - ${error.message}`)
@@ -99,7 +109,7 @@ async function tunnelToTest (familyName) {
     const cli = getPayCliPath() + '/dist/bin/cli.js'
     process.env.BASTION_TASK_DEF_NAME = familyName
     debug(`Setting BASTION_TASK_DEF_NAME env variable to ${familyName}`)
-    const tunnelCommand = spawn('node', [cli, 'tunnel', 'test-12', 'connector'], { timeout: 200000 })
+    tunnelCommand = spawn('node', [cli, 'tunnel', 'test-12', 'connector'], { timeout: 200000 })
 
     tunnelCommand.stderr.on('data', (data) => {
       try {
@@ -111,6 +121,9 @@ async function tunnelToTest (familyName) {
     tunnelCommand.stdout.on('data', (data) => {
       try {
         checkAndActOnTunnelOutput(tunnelCommand, data)
+        if (tunnelSuccessful) {
+          resolve()
+        }
       } catch (e) {
         reject(e)
       }
@@ -129,6 +142,71 @@ async function tunnelToTest (familyName) {
   })
 }
 
+async function testDBConnection () {
+  try {
+    log('..... Testing DB connection .....')
+    const environment = 'test-12', database = 'connector'
+    const dbUser = await getParameterValue(`${environment}_${database}.db_support_user_readonly`)
+    const dbPassword = await getParameterValue(`${environment}_${database}.db_support_password_readonly`)
+    debug(`Connecting as DB user - ${dbUser}`)
+    const client = new Client({
+      user: dbUser,
+      password: dbPassword,
+      host: '127.0.0.1',
+      port: 65432,
+      database: database,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    })
+
+    await client.connect()
+    const result = await client.query('SELECT NOW() as now')
+    debug(`DB query result: ${JSON.stringify(result.rows, null, 2)}`)
+
+    if (result && result.rows && result.rows.length === 1) {
+      dbConnectionSuccessful = true
+      log('DB connection successful!')
+    } else {
+      log('Error executing DB query')
+    }
+    await client.end()
+  } catch (e) {
+    log(`Error connecting to database - ${e.message}`)
+    throw e
+  }
+}
+
+async function getParameterValue (parameter) {
+  debug(`Getting parameter [${parameter}] from Parameter Store`)
+  const command = new GetParametersCommand({ Names: [parameter], WithDecryption: true })
+  const response = await ssmClient.send(command)
+
+  if (response && response.Parameters && response.Parameters.length > 0) {
+    return response.Parameters[0].Value
+  }
+  throw new Error(`Error getting parameter [${parameter}] from Parameter Store`)
+}
+
+async function checkBastionTaskStatus () {
+  if (familyName) {
+    log('..... Checking for active bastion tasks .....')
+    const input = {
+      cluster: 'test-12-fargate',
+      family: familyName,
+      desiredStatus: 'RUNNING'
+    }
+    const command = new ListTasksCommand(input)
+    const response = await ecsClient.send(command)
+    debug(`Bastion tasks for task ${familyName} - ${JSON.stringify(response, null, 2)}`)
+
+    if (response && response.taskArns && response.taskArns.length === 0) {
+      bastionShutdown = true
+      log('No active bastion tasks!')
+    }
+  }
+}
+
 function checkAndActOnTunnelOutput (command, data) {
   if (!data || data === '.\n') {
     return
@@ -145,10 +223,6 @@ function checkAndActOnTunnelOutput (command, data) {
 
   if (data.includes('Waiting for connections')) {
     tunnelSuccessful = true
-    //todo: test DB connection
-
-    log('End of test. Exiting tunnel...')
-    command.stdin.write('exit\n')
   }
 
   if (data.includes('Starting session with SessionId')) {
@@ -191,28 +265,58 @@ function validateEnvVars () {
   }
 }
 
+async function createTaskDefAndTunnel () {
+  const imageTag = process.env.IMAGE_TAG
+
+  familyName = await createTaskDefinition(imageTag)
+  await tunnelToTest(familyName)
+}
+
+async function exitTunnelAndCleanup () {
+  log('.... Cleaning up ....')
+  if (tunnelCommand) {
+    tunnelCommand.stdin.write('exit\n')
+    debug('Closed tunnel...')
+  }
+  if (familyName) {
+    debug(`Deleting task definition`)
+    await deleteTaskDefinition(familyName)
+  }
+}
+
 async function run () {
-  let familyName
   try {
     validateEnvVars()
-    const imageTag = process.env.IMAGE_TAG
 
-    familyName = await createTaskDefinition(imageTag)
     await buildPayCLI()
-    await tunnelToTest(familyName)
-    await deleteTaskDefinition(familyName)
+    await createTaskDefAndTunnel()
+
+    if (tunnelSuccessful) {
+      await testDBConnection()
+    }
+
+    await exitTunnelAndCleanup()
+
+    await new Promise(resolve => setTimeout(resolve, 2000)) // wait for bastion to terminate and AWS to return correct status
+    await checkBastionTaskStatus()
 
     if (!tunnelSuccessful) {
       log('Failed to tunnel to database')
       process.exit(1)
     }
+    if (!dbConnectionSuccessful) {
+      log('Failed to connect to connector database')
+      process.exit(1)
+    }
+    if (!bastionShutdown) {
+      log('Bastion has not been shutdown after the session has been terminated')
+      process.exit(1)
+    }
+    log('Bastion E2E test completed successfully!')
   } catch (e) {
     log(`----- Error testing bastion -----`)
     debug(e)
-    if (familyName) {
-      log(`Deleting task definition`)
-      await deleteTaskDefinition(familyName)
-    }
+    await exitTunnelAndCleanup()
     process.exit(1)
   }
 }
