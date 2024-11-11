50a483d4b3b9a0401da5ed346c62de2b6a0bd7f1
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2024-11-11 15:12:53 +0000

    PP-12922: Update script

--- ci/scripts/ad-hoc-sql.sh
+++ ci/scripts/ad-hoc-sql.sh
@@ -1,13 +1,25 @@
-#!/usr/bin/env ash
-# shellcheck shell=dash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-echo "START"
-cd ad-hoc-sql-scripts/
+NUMBER_OF_DIRS=0
+WORKING_DIR=
+
+while read -r DIR; do
+  NUMBER_OF_DIRS=$((NUMBER_OF_DIRS+1))
+  WORKING_DIR="$DIR"
+  echo ". $DIR"
+done < <(find ad-hoc-sql-scripts -name 'ZD-*' -type d)
+
+if [[ "$NUMBER_OF_DIRS" -eq 0 ]]; then
+  echo "No Zendesk ticket directories found. Exiting"
+  exit 0
+elif [[ "$NUMBER_OF_DIRS" -ge 2 ]]; then
+  echo "More than 1 Zendesk ticket directories found. Exiting"
+  exit 0
+fi
+
+echo "Changing to directory $WORKING_DIR"
+cd "$WORKING_DIR"
 
 ls -la
-
-echo "MORE"
-
-tree
