460bce928249e939e2f034b5d43aca1f0900bac9
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2024-12-04 09:45:02 +0000

    PP-13222 Jobs for support notifications (#1316)
    - Added jobs to notify support people about triaging alerts during the weekend & to update support notes

--- ci/scripts/support-rota-notification/slack-client.js
+++ ci/scripts/support-rota-notification/slack-client.js
@@ -9,17 +9,38 @@ async function setSlackChannelTopic (channelId, newTopic) {
     });
 
     if (response.ok) {
-      console.log('Channel topic updated successfully.');
+      console.log('Channel topic updated successfully.')
     } else {
-      console.error(`Error: ${response.error}`);
+      console.error(`Error: ${response.error}`)
       process.exit(1)
     }
   } catch (error) {
-    console.error(`Error setting slack channel topic: ${error.message}`);
+    console.error(`Error setting slack channel topic: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+async function sendSlackNotification (channelId, text) {
+  const web = new WebClient(process.env.SLACK_SECRET)
+  try {
+    const response = await web.chat.postMessage({
+      channel: channelId,
+      text: text
+    })
+
+    if (response.ok) {
+      console.log('Posted message successfully.')
+    } else {
+      console.error(`Error response received posting message to slack channel : ${response.error}`)
+      process.exit(1)
+    }
+  } catch (error) {
+    console.error(`Error posting message to slack channel: ${error.message}`)
     process.exit(1)
   }
 }
 
 module.exports = {
-  setSlackChannelTopic
+  setSlackChannelTopic,
+  sendSlackNotification
 }
