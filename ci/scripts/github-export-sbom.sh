1bc5d7ef188f3c1dfbac113d9869d99f2298b2b6
Manish Lad <manish.lad@digital.cabinet-office.gov.uk>
2025-03-17 17:28:58 +0000

    Run curl command in silent mode to reduce pipeline output noise

--- ci/scripts/github-export-sbom.sh
+++ ci/scripts/github-export-sbom.sh
@@ -16,6 +16,7 @@ for repo do
         -H "Authorization: Bearer $GITHUB_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/${OWNER}/"${repo}"/dependency-graph/sbom \
+        --silent \
         --output "${file}"
 done
 
